821b6ed938acee122483dc93864bd08a5ef8c33d
Arseny <cheplucov32@gmail.com>
2022-02-02 18:41:24 +0300

    Add flask-sqlalchemy

--- flaskpractice.py
+++ flaskpractice.py
@@ -1,6 +1,23 @@
+#import libraries
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
+# configure flask
 app = Flask(__name__)
+#configure flask database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' # can be sqlite:////tmp/test.db
+db = SQLAlchemy(app)
+
+# database class: create columns, initialize them, output data 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+    def __repr__(self):
+        return '<User %r >' % self.username
 
 @app.route("/")
 def index():
